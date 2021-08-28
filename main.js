e165d46cfc0375bb3cdc54a4af7890e9acef1d0c
Johnny <johnnylabedzki@hotmail.com>
2021-08-28 12:01:20 -0700

    Created a constants file which exports all the needed variable names.

---
+++ main.js
@@ -0,0 +1,5 @@
+const { userGrid, computerGrid, shipGrid, ships, destroyer, submarine, cruiser, battleship, carrier, startGame, rotateShips, turn, messageLog } = require('./src/constants');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const userGrid = $('.userGrid');
+const computerGrid = $('.computerGrid');
+const shipGrid = $('.shipGrid');
+const ships = $('.ship');
+const destroyer = $('.destroyerContainer');
+const submarine = $('.submarineContainer');
+const cruiser = $('.cruiserContainer');
+const battleship = $('.battleshipContainer');
+const carrier = $('.carrierContainer');
+const startGame = $('#start');
+const rotateShips = $('rotateShips');
+const turn = $('#whoseTurn');
+const messageLog = $('#messageLog');
+
+
+module.exports = { userGrid, computerGrid, shipGrid, ships, destroyer, submarine, cruiser, battleship, carrier, startGame, rotateShips, turn, messageLog };
