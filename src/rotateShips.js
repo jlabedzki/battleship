01f99ce35df448e0d4282a626277db3d18f20819
Johnny <johnnylabedzki@hotmail.com>
2021-08-29 12:26:17 -0700

    Completed the rotateShips.js functionality. Also added a pulsing turn indicator to be used when implementing the turn based functionality. Next step is to implement drag and drop features for the player ships.

--- src/rotateShips.js
+++ src/rotateShips.js
@@ -1,30 +1,37 @@
+//Since the page loads with the ships horizontally, we set the default to horizontal
 let horizontal = true;
 
-const rotateShips = (ship) => {
+
+//Takes in a ship parameter, which is a variable referencing the ships container element in the html file. Also takes in the name of the ship as a string so that we can toggle the class. If horizontal is true, then we change the class of the ship to it's vertical counterpart and vise versa.
+const rotateShips = (ship, name) => {
   if (horizontal) {
-    ship.toggleClass(`destroyerContainerVertical`)
+    ship.toggleClass(`${name}ContainerVertical`)
     horizontal = false;
+  }
+  if (!horizontal) {
+    ship.toggleClass(`${name}Container`);
+    horizontal = true;
   }
 };
 
 
 destroyer.click(() => {
-  rotateShips(destroyer);
+  rotateShips(destroyer, 'destroyer');
 });
 
 submarine.click(() => {
-
+  rotateShips(submarine, 'submarine');
 });
 
 cruiser.click(() => {
-
+  rotateShips(cruiser, 'cruiser');
 });
 
 battleship.click(() => {
-
+  rotateShips(battleship, 'battleship');
 });
 
 carrier.click(() => {
-
+  rotateShips(carrier, 'carrier');
 });
 
